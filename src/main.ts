108ee6f506bb6b04f51a3dfb3600be1d6650d116
d.njeru <wambu43@gmail.com>
2024-02-17 12:03:00 +0300

    Ft: Added https support on backend

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('cert.key'),
+    cert: fs.readFileSync('cert.crt'),
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     allowedHeaders: '*',
